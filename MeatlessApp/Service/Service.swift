04396d8037012b626b3a9552608fc762f97372ac
Natália Sapucaia <natisapucaia@gmail.com>
2024-03-14 15:38:17 -0300

    make api request

--- MeatlessApp/Service/Service.swift
+++ MeatlessApp/Service/Service.swift
@@ -3,8 +3,24 @@ import Foundation
 struct Service {
     
     func fetchRestaurantList(completion: @escaping ([Restaurant]?)->Void) {
-        
-        // STORY 1: Implement the API integration to fetch restaurant list.
-         
+        guard let url = URL(string: "https://raw.githubusercontent.com/devpass-tech/meatless-api/main/restaurant_list.json") else {
+            return
+        }
+
+        let request = URLRequest(url: url)
+        let session = URLSession.shared
+        let task = session.dataTask(with: request) { data, response, error in
+            if let error = error {
+                return
+            }
+            guard let httpResponse = response as? HTTPURLResponse,
+                  200..<300 ~= httpResponse.statusCode else {
+                return
+            }            
+            guard let data = data else {
+                return
+            }
+        }
+        task.resume()
     }
 }
